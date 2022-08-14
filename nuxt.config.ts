3c210f2ce9e7809dc74353960b3e70a9361b6b49
bob <jacques.r@myplanet.com>
2022-08-13 22:41:21 -0400

    blog setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  generate: {
+    routes: function () {
+      const fs = require("fs");
+      const path = require("path");
+      return fs.readdirSync("./content/blog").map((file) => {
+        return {
+          route: `/blog/${path.parse(file).name}`, // Return the slug
+          payload: require(`./content/blog/${file}`),
+        };
+      });
+    },
+  },
+  modules: ["@nuxt/content"],
+  content: {
+    // Options
+  },
+  target: "static",
+});
